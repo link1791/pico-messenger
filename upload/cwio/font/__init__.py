fab8a8a0f70d35af5a891f7d12b836c95dae8d63
Z User <z@container>
2026-07-06 23:00:49 +0000

    Pico Messenger - web relay server

--- upload/cwio/font/__init__.py
+++
@@ -1,5 +0,0 @@
-from .font import *
-from .classwiz_cw import Font as classwiz_cw
-from .miniwi import Font as miniwi
-
-all = [classwiz_cw, miniwi]
--- upload/garbage.py
+++
@@ -1,8 +0,0 @@
-import gc
-
-def collect():
-    old = gc.mem_free()
-    gc.collect()
-    new = gc.mem_free()
-    diff = new - old
-    print("[GRBG] Freed up " + (str(diff)) + " bytes of space")
